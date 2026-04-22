0e5bfde8a21d489b95a5d7147251ff7409d4a85b
Pietro <pietroaugusto051208@gmail.com>
2026-04-22 15:10:57 -0300

    Endpoint Login

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-//cadastro//////////////
+//cadastro////////////// - Arthur Silveira
 app.post ("/cadastro", async (req, res) => {
   const { email, password, name, rm, curso, funcao, telnumero,  } = req.body
 
@@ -26,6 +26,24 @@ app.post ("/cadastro", async (req, res) => {
   })
 
   return res.status(201).json(user)
+
+})
+
+//login////////////// - Pietro Augusto
+app.post("/login", async (req, res) => {
+  const { rm, cpf, email, password } = req.body
+
+  const user = await prisma.user.findFirst({ where: { rm || cpf || email } })
+
+  if (!user) {
+    return res.status(404).json({ error: "usuario não encontrado" })
+  }
+
+  if (!(await bcrypt.compare(password, user.password))){
+    return res.status(401).json({ error: "Credenciais inválidas" })
+  }
+
+  return res.status(200).json("Login realizado com sucesso!")
 
 })
 
